53f95cebc6d38286551ca816b7d9d6e4e386f501
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-10-20 16:13:41 +0400

    xervmon install script work: ip detection, interface, configure package

--- xervmon_install.py
+++ xervmon_install.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import platform
+import fileinput
 import urllib2
 import urllib
 import re
@@ -15,7 +16,9 @@ import logging
 
 from optparse import OptionParser, make_option
 
-DEFAULT_TENNANT = 'devmaas'
+DEFAULT_tenant = 'devmaas'
+DEBIAN_LIKE_SYSTEMS = ["Debian", "Mint", "Ubuntu"]
+DEFAULT_INTERFACE = 'eth0'
 TMP_FILE = '/tmp/tesspackage'
 URL_SCHEME = 'http'
 URL_DOMAIN = 'xervmon.com'
@@ -34,9 +37,19 @@ URL_METHODS = {
 DEB_PACKAGE = 'https://github.com/sseshachala/xervmonbroker_agents/raw/master/distros/xervmon-broker-agent_1.2.3i2-2_all.deb'
 RPM_PACKAGE = 'https://github.com/sseshachala/xervmonbroker_agents/raw/master/distros/xervmon_broker-agent-1.2.3i2-1.noarch.rpm'
 
+AGENT_CONFIG = '/etc/xinetd.d/xervmon_broker'
 
-def make_api_url(tennant, method, params):
-    netloc = '%s.%s' % (tennant, URL_DOMAIN)
+
+def get_interface_ip(ifname):
+    import fcntl
+    import struct
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+                            ifname[:15]))[20:24])
+
+
+def make_api_url(tenant, method, params):
+    netloc = '%s.%s' % (tenant, URL_DOMAIN)
     url_method = URL_METHODS.get(method)
     if url_method is None:
         # logger.error("No such method %s" % method)
@@ -74,7 +87,7 @@ def make_api_call(url, api_key):
         res = opener.open(url)
     except urllib2.HTTPError, e:
         # logger.error(str(e))
-        raise
+        print 'Couldnt authenticate. Please try another api key or contact our support'
         return
     try:
         return json.loads(res.read())
@@ -83,41 +96,83 @@ def make_api_call(url, api_key):
 
 
 def get_package(dist):
-    if dist in ["Debian", "Mint", "Ubuntu"]:
+    if dist in DEBIAN_LIKE_SYSTEMS:
         package = DEB_PACKAGE
     else:
         package = RPM_PACKAGE
     return package
 
 
-def get_install_command(dist):
-    if dist in ["Debian", "Mint", "Ubuntu"]:
-        command = "sudo dpkg -i %s"
+def get_package_install_command(dist):
+    if dist in DEBIAN_LIKE_SYSTEMS:
+        command = "apt-get install %s"
     else:
-        command = "sudo yum install %s"
+        command = "yum install %s"
     return command
 
 
-def install_package():
-    system = platform.system()
-    if system != 'Linux':
-        print ("We are sorry. We do not support %s. Currently support is only \
-            for Linux" % system)
-        sys.exit()
-    dist = platform.linux_distribution()[0]
+def get_install_command(dist):
+    if dist in DEBIAN_LIKE_SYSTEMS:
+        command = "dpkg -i %s"
+    else:
+        command = "yum install %s"
+    return command
+
+
+def configure(broker_ip, dist):
+    added = False
+    for line in fileinput.input(AGENT_CONFIG, inplace=1):
+        if 'only_from' in line:
+            if not added:
+                print ('\tonly_from = %s\n' % broker_ip),
+                added = True
+        else:
+            print line,
+    if dist not in DEBIAN_LIKE_SYSTEMS:
+        subprocess.call('chkconfig xinetd on')
+
+
+def install_package(installer, package):
+    command = installer % package
+    p1 = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
+    output = p1.communicate()[0]
+    print output
+
+
+def install(dist):
     package = get_package(dist)
     resp = urllib2.urlopen(package)
     with open(TMP_FILE, 'w') as fp:
         fp.write(resp.read())
-    command = get_install_command(dist) % TMP_FILE
-    p1 = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
-    output = p1.communicate()[0]
-    print output
+    installer = get_install_command(dist)
+    package_installer = get_package_install_command(dist)
+    install_package(package_installer, 'xinetd')
+    if dist not in DEBIAN_LIKE_SYSTEMS:
+        install_package(package_installer, 'chkconfig')
+    install_package(installer, TMP_FILE)
     return True
+
+
+def get_from_input(msg):
+    var = raw_input('%s\n' % msg)
+    var = var.strip()
+    return var
 
 
 def main():
     """Main control function"""
+
+    system = platform.system()
+    dist = platform.linux_distribution()[0]
+    if system != 'Linux':
+        print ("We are sorry. We do not support %s. Currently support is only \
+            for Linux" % system)
+        sys.exit()
+
+    run_user = os.getenv("USER")
+    if run_user != 'root':
+        print 'Please run script as a root'
+        sys.exit()
 
     usage = "usage: %prog [options]"
     option_list = [
@@ -127,22 +182,36 @@ def main():
                 dest="user"),
             make_option("-H", "--host", action="store", type="string",
                 dest="host"),
-            make_option("-t", "--tennant", action="store", type="string",
-                dest="tennant", default=DEFAULT_TENNANT),
+            make_option("-i", "--interface", action="store", type="string",
+                dest="interface", help="default interface to detect ip if no \
+                host given", default=DEFAULT_INTERFACE),
+            make_option("-t", "--tenant", action="store", type="string",
+                dest="tenant", default=DEFAULT_tenant),
             ]
     parser = OptionParser(usage, option_list=option_list)
     (options, args) = parser.parse_args()
     key = options.key
     user = options.user
     host = options.host
-    tennant = options.tennant
+    if host is not None and not check_ip(host):
+        print "Please enter valid ip in host parameter."
+        sys.exit()
+
+    if host is None:
+        try:
+            host = get_interface_ip(options.interface)
+        except StandardError:
+            print 'Couldnt detect ip for interface %s' % options.interface
+            sys.exit()
+
+    tenant = options.tenant
     if key is None:
-        key = raw_input("Enter api key\n")
+        key = get_from_input("Enter api key")
     if user is None:
-        user = raw_input("Enter your username\n")
+        user = get_from_input("Enter your username")
 
     base_params = {'key': key, 'username': user}
-    auth_url = make_api_url(tennant, 'auth', base_params)
+    auth_url = make_api_url(tenant, 'auth', base_params)
     auth_res = make_api_call(auth_url, key)
     if auth_res is None:
         print 'Error making auth api call'
@@ -150,20 +219,17 @@ def main():
     if auth_res['response'] == 'false':
         print auth_res['error']
         sys.exit()
+    broker_ip = auth_res['broker_ip']
 
-    if host is None:
-        host = raw_input("Please enter your public host\n")
-        if not check_ip(host):
-            print "Please enter valid ip"
-            sys.exit()
-    res_install = install_package()
+    res_install = install(dist)
+    configure(broker_ip, dist)
     if not res_install:
         print "Couldnt install package"
         sys.exit()
 
     enable_params = base_params.copy()
     enable_params.update({'host': host})
-    enable_url = make_api_url(tennant, 'enable', enable_params)
+    enable_url = make_api_url(tenant, 'enable', enable_params)
     enable_res = make_api_call(enable_url, key)
     if enable_res is None or enable_res['response'] == 'false':
         print "Error enabling host"
